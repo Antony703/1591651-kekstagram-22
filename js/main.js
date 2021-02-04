a67dacb3d1a572be7a9317b3b3e624468167f36b
Antony7032 <l703u@yandex.ru>
2021-02-05 00:42:40 +0700

    small_fixes

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
-const getRandomNatural = function(min, max) {
-  if (min >= 0 && max >= 0 && max > min) {
+const getRandomNatural = function (min, max) {
+  if (max >= 0 && max > min) {
+    if (min < 0) {
+      min = 0;
+    }
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
   }
   return 0;
 }
-const checkStringLength = function (inputString, maxLength) {
+const isStringLengthCorrect = function (inputString, maxLength) {
   if (inputString.length <= maxLength)
     return true;
   return false;
 }
-checkStringLength ('test',4);
-getRandomNatural (5,20);
+isStringLengthCorrect('test', 4);
+getRandomNatural(5, 20);
